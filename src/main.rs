d1e9c4818fb51ea5dfc408ec6cc1c1c67589f823
Trevor Reiff <trevorreiff@gmail.com>
2017-08-18 20:18:55 -0400

    Add basic logic to parse cli input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[3];
+
+    println!("Searching for query {}, in file {}.", query, filename);
 }
